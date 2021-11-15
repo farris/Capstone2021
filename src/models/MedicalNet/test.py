7ca5db0a39344702fd6825aad96e36cdd779c2c3
nasseralrayes <nos.alrayes@gmail.com>
2021-11-14 22:12:27 -0500

    adding my changes to medicalnet

--- src/models/MedicalNet/test.py
+++ src/models/MedicalNet/test.py
@@ -1,3 +1,4 @@
+%%writefile  MedicalNet/test.py
 from setting import parse_opts 
 from datasets.brains18 import BrainS18Dataset
 from model import generate_model
@@ -11,39 +12,10 @@ import sys
 import os
 from utils.file_process import load_lines
 import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+from skimage import io
 
-
-def seg_eval(pred, label, clss):
-    """
-    calculate the dice between prediction and ground truth
-    input:
-        pred: predicted mask
-        label: groud truth
-        clss: eg. [0, 1] for binary class
-    """
-    Ncls = len(clss)
-    dices = np.zeros(Ncls)
-    [depth, height, width] = pred.shape
-    for idx, cls in enumerate(clss):
-        # binary map
-        pred_cls = np.zeros([depth, height, width])
-        pred_cls[np.where(pred == cls)] = 1
-        label_cls = np.zeros([depth, height, width])
-        label_cls[np.where(label == cls)] = 1
-
-        # cal the inter & conv
-        s = pred_cls + label_cls
-        inter = len(np.where(s >= 2)[0])
-        conv = len(np.where(s >= 1)[0]) + inter
-        try:
-            dice = 2.0 * inter / conv
-        except:
-            print("conv is zeros when dice = 2.0 * inter / conv")
-            dice = -1
-
-        dices[idx] = dice
-
-    return dices
 
 def test(data_loader, model, img_names, sets):
     masks = []
@@ -59,18 +31,15 @@ def test(data_loader, model, img_names, sets):
 
         # resize mask to original size
         [batchsize, _, mask_d, mask_h, mask_w] = probs.shape
-        data = nib.load(os.path.join(sets.data_root, img_names[batch_id]))
-        data = data.get_data()
+        data = io.imread(os.path.join(sets.data_root, img_names[batch_id]))
         [depth, height, width] = data.shape
         mask = probs[0]
         scale = [1, depth*1.0/mask_d, height*1.0/mask_h, width*1.0/mask_w]
-        mask = ndimage.interpolation.zoom(mask, scale, order=1)
-        mask = np.argmax(mask, axis=0)
-        
+        mask = ndimage.interpolation.zoom(mask.cpu(), scale, order=1)
+        mask = np.argmax(mask, axis=0)   
         masks.append(mask)
  
     return masks
-
 
 if __name__ == '__main__':
     # settting
@@ -84,23 +53,25 @@ if __name__ == '__main__':
     net.load_state_dict(checkpoint['state_dict'])
 
     # data tensor
-    testing_data =BrainS18Dataset(sets.data_root, sets.img_list, sets)
+    testing_data = BrainS18Dataset(sets.data_root, sets.img_list, sets)
     data_loader = DataLoader(testing_data, batch_size=1, shuffle=False, num_workers=1, pin_memory=False)
 
     # testing
     img_names = [info.split(" ")[0] for info in load_lines(sets.img_list)]
     masks = test(data_loader, net, img_names, sets)
     
-    # evaluation: calculate dice 
+    # evaluation: calculate accuracy 
     label_names = [info.split(" ")[1] for info in load_lines(sets.img_list)]
     Nimg = len(label_names)
-    dices = np.zeros([Nimg, sets.n_seg_classes])
+    accuracies = np.zeros([Nimg, sets.n_seg_classes])
     for idx in range(Nimg):
-        label = nib.load(os.path.join(sets.data_root, label_names[idx]))
-        label = label.get_data()
-        dices[idx, :] = seg_eval(masks[idx], label, range(sets.n_seg_classes))
+        label = np.load(os.path.join(sets.data_root, label_names[idx]))
+        label = label.f.arr_0
+        print('masks[idx]', np.max(masks[idx]), np.min(masks[idx]))
+        accuracies[idx, :] = accuracy_score(masks[idx].flatten(), label.flatten())
     
     # print result
     for idx in range(1, sets.n_seg_classes):
-        mean_dice_per_task = np.mean(dices[:, idx])
-        print('mean dice for class-{} is {}'.format(idx, mean_dice_per_task))   
+        print(accuracies)
+        mean_accuracy_per_task = np.mean(accuracies[idx, :])
+        print('mean accuracy for class-{} is {}'.format(idx, mean_accuracy_per_task))

149a3e85ce289b2bca9a5c053df9bda2cc385fb2
Eric He <eriqqc@gmail.com>
2021-12-05 10:54:32 -0500

    add transforms to the dataloader

--- src/data/torch_utils.py
+++ src/data/torch_utils.py
@@ -10,13 +10,14 @@ class MonkeyEyeballsDataset(Dataset):
   Loads PyTorch arrays of the monkey eyeballs dataset. 
   """
 
-  def __init__(self, data_dir, labels_df):
+  def __init__(self, data_dir, labels_df, transform=None):
     """
     data_dir: path to image scans directory
     labels_df: pandas dataframe holding IOP, ICP and internal Scan ID value
     """
     self.data_dir = data_dir 
     self.labels_df = labels_df
+    self.transform = transform
 
   def __getitem__(self, idx):
     if torch.is_tensor(idx):
@@ -30,6 +31,9 @@ class MonkeyEyeballsDataset(Dataset):
         'scan':torch.load(scan_path),
         'id': self.labels_df.iloc[idx]['id']
     }
+
+    if self.transform:
+        sample['scan'] = self.transform(sample['scan'].unsqueeze(0)).squeeze()
 
     return sample
 
